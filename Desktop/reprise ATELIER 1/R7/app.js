dc0b29768794c07aa5d3c780b708fd18fb6de7c8
Mbissouu <kassepapambissane@gmail.com>
2024-08-01 13:34:58 +0000

    commit7

---
+++ Desktop/reprise ATELIER 1/R7/app.js
@@ -0,0 +1,40 @@
+class Todos extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            inputTudo:"",
+            Todos: []
+        }
+    }
+    render(){
+        return (
+            <div className="container mt-4">
+                <h1>To-Do List</h1>
+                <p>{this.state.inputTodo}</p>
+                <div className="input-group mb-3">
+                <input
+                type="text"
+                value={this.state.inputTodo}
+                onChange={(e) =>{
+                  this.setState({inputTodo: e.target.value})
+                }}
+                className="form-control"
+                placeholder="Ajoutez une tâche"
+                />
+              <button className="btn btn-primary"  onClick={()=>{
+                  this.setState({todos:[...this.state.todos, this.state.inputTodo]})
+              }}>Ajouter</button>   
+                </div>
+                <ul className="list-group">
+                    {
+                        this.state.Todos.map((todo)=>{
+                            return <li>{todo}</li>
+                        })
+                    }
+                </ul>
+            </div>
+        );
+
+    }
+}
+ReactDOM.render(<Todos/>, document.getElementById("root"))
